1d6d1710d68d4f21a4686d71284c4f0ab288ef63
wwgesd1 <112858140+wwgesd1@users.noreply.github.com>
2022-09-15 17:31:49 +0900

    feat:AddressPool 추가
    PoolInfoSet으로 풀생성후 Call

--- Assets/Scripts/Utility/AddressObject.cs
+++ Assets/Scripts/Utility/AddressObject.cs
@@ -8,52 +8,106 @@ using static UnityEditor.FilePathAttribute;
 
 public static class AddressObject
 {
-
     public static List<GameObject> Instinates(AssetLabelReference label)
     {
         List<GameObject> objectList = new List<GameObject>();
-        var locations = Addressables.LoadResourceLocationsAsync(label.labelString).WaitForCompletion();
+        var locations = Locations(label);
         foreach (var location in locations)
         {
             objectList.Add(Addressables.InstantiateAsync(location).WaitForCompletion());
         }
         return objectList;
-    }
-
-    public static List<GameObject> LimitInstinates(AssetLabelReference label, int limit)
+    } // 라벨에 있는거 전부
+    public static List<GameObject> LimitInstinates(AssetLabelReference label, int limit) // 라벨에 있는거 일부만,라벨 전체갯수보다 많을경우 무시
     {
         List<GameObject> objectList = new List<GameObject>();
-        var locations = Addressables.LoadResourceLocationsAsync(label.labelString).WaitForCompletion();
+        var locations = Locations(label);
         for (int i = 0; i < limit; i++)
         {
             if(locations.Count > 0)
             {
-                IResourceLocation location = locations[Random.Range(0, locations.Count)];
+                var location = locations[Random.Range(0, locations.Count)];
                 objectList.Add(Addressables.InstantiateAsync(location).WaitForCompletion());
                 locations.Remove(location);
             }
         }
         return objectList;
     }
-
     public static List<GameObject> LimitRandomInstinates(AssetLabelReference label,int limit)
     {
         List<GameObject> objectList = new List<GameObject>();
-        var locations = Addressables.LoadResourceLocationsAsync(label.labelString).WaitForCompletion();
-        for (int i = 0; i < limit; i++)
+        var locations = RandomLoactions(label,limit);
+        foreach (var location in locations)
         {
-            objectList.Add(Addressables.InstantiateAsync(locations[Random.Range(0, locations.Count)]).WaitForCompletion());
+            objectList.Add(Addressables.InstantiateAsync(location).WaitForCompletion());
         }
         return objectList;
-    }
-
-
+    } // 라벨에 있는거 랜덤하게, 갯수만큼, 중복가능
     public static GameObject RandomInstinate(AssetLabelReference label)
     {
-        var locations = Addressables.LoadResourceLocationsAsync(label.labelString).WaitForCompletion();
-        return Addressables.InstantiateAsync(locations[Random.Range(0, locations.Count)]).WaitForCompletion();
+        var location = RandomLocation(label);
+        return Addressables.InstantiateAsync(location).WaitForCompletion();
+    } // 라벨에 있는거 랜덤하게 하나 
+    public static GameObject Instinate(IResourceLocation location)
+    {
+        return Addressables.InstantiateAsync(location).WaitForCompletion();
+    }
+    public static List<GameObject> Instinates(List<IResourceLocation> locations)
+    {
+        List<GameObject> gameObjects = new List<GameObject>();
+        foreach(var location in locations)
+        {
+            gameObjects.Add(Addressables.InstantiateAsync(location).WaitForCompletion());
+        }
+        return gameObjects;
     }
 
+    public static GameObject GameObjectSet(AssetLabelReference label)
+    {
+        return Addressables.LoadAssetAsync<GameObject>(label).WaitForCompletion();
+    }
+    public static List<GameObject> GameObjectsSet(AssetLabelReference label)
+    {
+        var gameObjects = new List<GameObject>();
+        var locations = Locations(label);
+        foreach(var location in locations)
+        {
+            gameObjects.Add(Addressables.LoadAssetAsync<GameObject>(label).WaitForCompletion());
+        }
+        return gameObjects;
+    }
+
+
+
+
+
+    #region Location
+    public static IResourceLocation RandomLocation(AssetLabelReference label)
+    {
+        var locations = Locations(label);
+        return locations[Random.Range(0, locations.Count)];
+    }
+    public static List<IResourceLocation> RandomLoactions(AssetLabelReference label,int locationNum)
+    {
+        List<IResourceLocation> resultLocations = new List<IResourceLocation>();
+        var locations = Locations(label);
+        for(int i = 0; i < locationNum; i++)
+        {
+            resultLocations.Add(locations[Random.Range(0, locations.Count)]);
+        }
+        return resultLocations;
+    }
+    public static IList<IResourceLocation> Locations(AssetLabelReference label)
+    {
+        List<IResourceLocation> locations = new List<IResourceLocation>();
+        var Ilocations = Addressables.LoadResourceLocationsAsync(label.labelString).WaitForCompletion();
+        foreach(var location in Ilocations)
+        {
+            locations.Add(location);
+        }
+        return locations;
+    }
+    #endregion
 
 
 
---
+++ Assets/Scripts/Utility/AdressObjectPool.cs
@@ -0,0 +1,183 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdressObjectPool : Singleton<AdressObjectPool>
+{
+    [System.Serializable]
+    public class PoolInfo
+    {
+        public GameObject poolObject;
+        public int start;
+        public int add;
+        public PoolInfo(GameObject _poolObject, int _start, int _add)
+        {
+            poolObject = _poolObject;
+            start = _start;
+            add = _add;
+        }
+    }
+    Dictionary<string, PoolInfo> poolInfoDic = new Dictionary<string, PoolInfo>();
+    Dictionary<PoolInfo, Queue<GameObject>> poolDic = new Dictionary<PoolInfo, Queue<GameObject>>();
+    Transform active;
+    Transform deactive;
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+        ActiveNoneActiveSet();
+    }
+    private void ActiveNoneActiveSet()
+    {
+        active = new GameObject("activePool").transform;
+        deactive = new GameObject("deactivePool").transform;
+        active.gameObject.SetActive(true);
+        deactive.gameObject.SetActive(false);
+    }
+
+    public void PoolInfoSet(GameObject gameObject, int start, int add)
+    {
+        if(PoolInfoSearch(gameObject)!=null)
+        {
+            return;
+        }
+        PoolInfo poolInfo = new PoolInfo(gameObject, start, add);
+        poolInfoDic.Add(gameObject.name, poolInfo);
+        poolDic.Add(poolInfo, new Queue<GameObject>());
+        for (int i = 0; i < poolInfo.start; i++)
+        {
+            GameObject go = Instantiate(poolInfo.poolObject, deactive);
+            go.SetActive(false);
+            poolDic[poolInfo].Enqueue(go);
+        }
+    }
+    public PoolInfo PoolInfoSearch(GameObject gameObject)
+    {
+        if(poolInfoDic.ContainsKey(gameObject.name))
+        {
+            return poolInfoDic[gameObject.name];
+        }
+        return null;
+    }
+
+    private void Add(PoolInfo poolInfo)
+    {
+        Queue<GameObject> queue = poolDic[poolInfo];
+        for(int i = 0; i < poolInfo.add; i++)
+        {
+            GameObject go = Instantiate(poolInfo.poolObject, deactive);
+            go.SetActive(false);
+            queue.Enqueue(go);
+        }
+    }
+
+    #region CallMethod
+    public Transform Call(PoolInfo poolInfo,Vector3 positon, Vector3 rotate)
+    {
+        Queue<GameObject> pool = poolDic[poolInfo];
+        if(pool.Count == 0)
+        {
+            Add(poolInfo);
+        }
+        Transform callObject = pool.Dequeue().transform;
+        callObject.position = positon;
+        callObject.rotation = Quaternion.Euler(rotate);
+        callObject.SetParent(active);
+        return callObject;
+    }
+    public Transform Call(PoolInfo poolInfo, Vector3 positon)
+    {
+        Queue<GameObject> pool = poolDic[poolInfo];
+        if (pool.Count == 0)
+        {
+            Add(poolInfo);
+        }
+        Transform callObject = pool.Dequeue().transform;
+        callObject.position = positon;
+        callObject.SetParent(active);
+        return callObject;
+    }
+
+    public Transform Call(PoolInfo poolInfo)
+    {
+        Queue<GameObject> pool = poolDic[poolInfo];
+        if (pool.Count == 0)
+        {
+            Add(poolInfo);
+        }
+        Transform callObject = pool.Dequeue().transform;
+        return callObject;
+    }
+    #endregion
+    public void Return(GameObject gameObject, PoolInfo poolInfo)
+    {
+        gameObject.transform.SetParent(deactive);
+        poolDic[poolInfo].Enqueue(gameObject);
+
+    }
+
+
+
+
+    //public void Add(string name)
+    //{
+    //    PoolInfo temp;
+    //    poolInfoDic.TryGetValue(name, out temp);
+    //    if (temp == null)
+    //    {
+    //        Debug.Log("풀 목록에 존재하지 않는 오브젝트");
+    //        return;
+    //    }
+    //    for (int i = 0; i < temp.add; i++)
+    //    {
+    //        InPoolObj tempobj = Instantiate(temp.obj, deactive);
+    //        tempobj.gameObject.SetActive(false);
+    //        poolDict[name + "(Clone)"].Enqueue(tempobj);
+    //    }
+    //}
+
+    //public void AddStart(string name)
+    //{
+    //    PoolInfo temp;
+    //    poolInfoDic.TryGetValue(name, out temp);
+    //    for (int i = 0; i < temp.start; i++)
+    //    {
+    //        InPoolObj tempobj = Instantiate(temp.obj, deactive);
+    //        tempobj.gameObject.SetActive(false);
+    //        poolDict[name + "(Clone)"].Enqueue(tempobj);
+    //    }
+    //}
+
+    //public Transform Call(string name, Vector3 positon, Vector3 rotate)
+    //{
+    //    if (!poolDict.ContainsKey(name + "(Clone)"))
+    //    {
+    //        return null;
+    //    }
+    //    if (poolDict[name + "(Clone)"].Count == 0)
+    //    {
+    //        Add(name);
+    //    }
+    //    Transform temp = poolDict[name + "(Clone)"]?.Dequeue().transform;
+    //    temp.position = positon;
+    //    temp.rotation = Quaternion.Euler(rotate);
+    //    temp.SetParent(active);
+    //    return temp;
+    //}
+
+    //public void Return(InPoolObj obj)
+    //{
+    //    if (!poolDict.ContainsKey(obj.gameObject.name))
+    //    {
+    //        return;
+    //    }
+    //    obj.transform.SetParent(deactive);
+    //    poolDict[obj.gameObject.name].Enqueue(obj);
+
+    //}
+
+
+
+
+}
